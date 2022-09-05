a10d6af751e3c5a8b59c188e4c438a2c19fe1332
Yqianyi <112791034+Yqianyi@users.noreply.github.com>
2022-09-05 21:03:30 +0800

    C Language First Lesson

---
+++ Test  1.c
@@ -0,0 +1,36 @@
+#include<stdio.h>
+
+int main()
+{
+	printf("Hello World\n");
+	return 0;
+}
+
+//包含一个叫stdio.h的文件
+//std-标准 standard input output(标准输入输出)
+//#include<stdio.h>
+//
+//int main()//主函数-程序的入口-main函数有且只有一个
+//{
+//	//这里完成任务
+//	//在屏幕上输出hello world
+//	//-print function--printf-打印函数
+//	//库函数-C语言本身提供给我们使用的函数
+//    //别人的东西-打招呼
+//	//#include（包含）
+//	printf("hello world\n");
+//	return 0;
+//}
+
+
+
+
+//int 是整形的意思
+//main前面的int表示main函数调用返回一个整型值
+//int main()
+//{
+//
+//
+	
+//	return 0;返回0
+//}
