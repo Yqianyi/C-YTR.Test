2ddb4aa769839d5c133c13993d3e973667948fb8
Yqianyi <112791034+Yqianyi@users.noreply.github.com>
2022-09-06 15:22:22 +0800

    The application of data types

---
+++ Test 2.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+
+int main()
+{
+    char ch = 'Y';
+    printf("%c\n", ch);
+
+    int age = 18;
+    printf("%d\n", age);
+
+    long num = 520;
+    printf("%d\n", num);
+
+    float f = 1314.0;
+    printf("%f\n", f);
+
+    double d = 13.14;
+    printf("%lf\n", d);
+
+    return 0;
+}
